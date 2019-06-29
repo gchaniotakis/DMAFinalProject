446207abce8fb6671b9af4d035987161d052d87b
George Chaniotakis <haniotinho@gmail.com>
2019-06-29 15:30:25 +0300

    first question completed

--- codes.R
+++ codes.R
@@ -1,1 +1,8 @@
-testing code
+##A##
+library("readxl")
+houses<-read_xls("C:/Users/George/Desktop/Data Mining Academy/Final_project/real_estate.xls")
+View (houses)
+
+which(is.na(houses))
+
+na.omit(houses)
